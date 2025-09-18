3fcbd600aac5bb52a1b9684cd052ac44c4848a4a
TheSleepyOwl <jiakefubao@gmail.com>
2025-09-18 14:14:22 +0300

    Create game.js

---
+++ Pacman/game.js
@@ -0,0 +1,137 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+const pacmanFrames = document.getElementById("animation");
+const ghostFrames = document.getElementById("ghosts");
+
+let createRect = (x, y, width, height, color) => {
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height);
+};
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
+let lives = 3;
+let ghostCount = 4;
+let ghostImageLocations = [
+  { x: 0, y: 0 },
+  { x: 176, y: 0 },
+  { x: 0, y: 121 },
+  { x: 176, y: 121 },
+];
+
+// Game variables
+let fps = 30;
+let pacman;
+let oneBlockSize = 20;
+let score = 0;
+let ghosts = [];
+let wallSpaceWidth = oneBlockSize / 1.6;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
+
+// we now create the map of the walls,
+// if 1 wall, if 0 not wall
+// 21 columns // 23 rows
+let map = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+  [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+  [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
+  [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+You sent
+if (j < map[0].length - 1 && map[i][j + 1] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth + wallOffset,
+            wallSpaceWidth,
+            wallInnerColor
+          );
+        }
+
+        if (i < map.length - 1 && map[i + 1][j] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColor
+          );
+        }
+
+        if (i > 0 && map[i - 1][j] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColor
+          );
+        }
+      }
+    }
+  }
+};
+
+let createGhosts = () => {
+  ghosts = [];
+  for (let i = 0; i < ghostCount * 2; i++) {
+    let newGhost = new Ghost(
+      9 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      10 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      oneBlockSize,
+      oneBlockSize,
+      pacman.speed / 2,
+      ghostImageLocations[i % 4].x,
+      ghostImageLocations[i % 4].y,
+      124,
+      116,
+      6 + i
+    );
+    ghosts.push(newGhost);
+  }
+};
+
+createNewPacman();
+createGhosts();
+gameLoop();
+
+window.addEventListener("keydown", (event) => {
+  let k = event.keyCode;
+  setTimeout(() => {
+    if (k == 37 || k == 65) {
+      // left arrow or a
+      pacman.nextDirection = DIRECTION_LEFT;
+    } else if (k == 38 || k == 87) {
+      // up arrow or w
+      pacman.nextDirection = DIRECTION_UP;
+    } else if (k == 39 || k == 68) {
+      // right arrow or d
+      pacman.nextDirection = DIRECTION_RIGHT;
+    } else if (k == 40 || k == 83) {
+      // bottom arrow or s
+      pacman.nextDirection = DIRECTION_BOTTOM;
+    }
+  }, 1);
+});
